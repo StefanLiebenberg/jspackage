20cbd174c125e06898a3946ea28cd06b687dfd57
Stefan Liebenberg <siga.fredo@gmail.com>
2016-02-26 09:08:51 +0100

    update soy

--- jspackage-closure-utilities/src/main/java/org/slieb/closure/javascript/internal/SoyNodeParser.java
+++ jspackage-closure-utilities/src/main/java/org/slieb/closure/javascript/internal/SoyNodeParser.java
@@ -1,36 +1,33 @@
 package org.slieb.closure.javascript.internal;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.template.soy.ErrorReporterImpl;
 import com.google.template.soy.SoyFileSetParser;
 import com.google.template.soy.base.internal.SoyFileSupplier;
-import com.google.template.soy.basetree.SyntaxVersion;
+import com.google.template.soy.error.ErrorReporter;
 import com.google.template.soy.shared.SoyAstCache;
-import com.google.template.soy.soyparse.ErrorReporter;
-import com.google.template.soy.soyparse.ErrorReporterImpl;
 import com.google.template.soy.soytree.SoyFileSetNode;
 import com.google.template.soy.types.SoyTypeRegistry;
 
-import java.util.List;
-
 public class SoyNodeParser {
-
-    private static final SyntaxVersion SYNTAX_VERSION = SyntaxVersion.V2_0;
 
     private static final ErrorReporter errorReporter = new ErrorReporterImpl();
 
-    private final List<SoyFileSupplier> soyFileSuppliers;
+    private final ImmutableMap<String, SoyFileSupplier> soyFileSuppliers;
 
     private final SoyTypeRegistry registry;
 
     private final SoyAstCache cache;
 
-    public SoyNodeParser(List<SoyFileSupplier> soyFileSuppliers) {
+    public SoyNodeParser(ImmutableMap<String, SoyFileSupplier> soyFileSuppliers) {
         this.soyFileSuppliers = soyFileSuppliers;
         this.registry = new SoyTypeRegistry();
         this.cache = new SoyAstCache();
     }
 
     public SoyFileSetNode parse() {
-        return new SoyFileSetParser(registry, cache, SYNTAX_VERSION, soyFileSuppliers, errorReporter).parse();
+        return new SoyFileSetParser(registry, cache, soyFileSuppliers, null, errorReporter)
+                .parse().fileSet();
     }
 }
 
